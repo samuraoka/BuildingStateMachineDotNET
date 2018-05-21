a5d7807659c33f148cd65b99dd58c0bdd3d3b815
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-21 19:22:37 +0900

    Initialized a project to demo Consuper-Producer Pattern.

---
+++ ConsumerProducer/ConsumerProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumerProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
