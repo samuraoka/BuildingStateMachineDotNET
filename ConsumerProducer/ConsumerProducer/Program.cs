3e13b537b16472ad8812d7a690cd663d150a2710
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-21 21:49:06 +0900

    Maked two producer threads work in the program.

--- ConsumerProducer/ConsumerProducer/Program.cs
+++ ConsumerProducer/ConsumerProducer/Program.cs
@@ -15,13 +15,27 @@ namespace ConsumerProducer
             var syncEvents = new SyncEvents();
 
             Console.WriteLine("Configuring worker threads...");
-            var producer = new Producer(queue, syncEvents);
+            // Chnage from original sample:
+            // Adding an additional producer to be closer to our scenario
+            var producer1 = new Producer(queue, syncEvents);
+            var producer2 = new Producer(queue, syncEvents);
             var consumer = new Consumer(queue, syncEvents);
-            var producerThread = new Thread(producer.ThreadRun);
-            var consumerThread = new Thread(consumer.ThreadRun);
+            var producerThread1 = new Thread(producer1.ThreadRun)
+            {
+                Name = "Producer1",
+            };
+            var producerThread2 = new Thread(producer2.ThreadRun)
+            {
+                Name = "Producer2",
+            };
+            var consumerThread = new Thread(consumer.ThreadRun)
+            {
+                Name = "Consumer",
+            };
 
             Console.WriteLine("Launching producer and consumer threads...");
-            producerThread.Start();
+            producerThread1.Start();
+            producerThread2.Start();
             consumerThread.Start();
 
             for (int i = 0; i < 4; i += 1)
@@ -33,7 +47,9 @@ namespace ConsumerProducer
             Console.WriteLine("Signaling threads to terminate...");
             syncEvents.ExitThreadEvent.Set();
 
-            producerThread.Join();
+            // Join all threads again to end gracefully
+            producerThread1.Join();
+            producerThread2.Join();
             consumerThread.Join();
         }
 
