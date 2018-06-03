3ffef77eb1068bca60f5417510e4b7fb9c141a65
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-04 01:31:53 +0900

    Implemented the StateMachineAction class.

---
+++ ActiveStateMachine/ActiveStateMachine/StateMachineAction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/*
+=========1=========2=========3=========4=========5=========6=========7=========8
+*/
+namespace ActiveStateMachine
+{
+    /// <summary>
+    /// Class for any actions in the state machine. It is used for guards,
+    /// transition actions as well as entry / exit actions
+    /// </summary>
+    public class StateMachineAction
+    {
+        /// <summary>
+        /// The name of this StateMachineAction
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// Delegate pointing to the implementation of method
+        /// to be executed
+        private Action _method;
+
+        /// <summary>
+        /// Constructor for state machine action
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="method"></param>
+        public StateMachineAction(string name, Action method)
+        {
+            Name = name;
+            _method = method;
+        }
+
+        /// <summary>
+        /// Method running the action.
+        /// Will be called e.g. by state machine, when a transition is
+        /// executed. Could also be used in a guard, entry or exit action.
+        /// </summary>
+        public void Execute()
+        {
+            _method.Invoke();
+        }
+    }
+}
+/*
+=========1=========2=========3=========4=========5=========6=========7=========8
+*/
