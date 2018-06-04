f5f25d84a1f4a6bfb57982818164ea0feaa6130f
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-04 10:02:15 +0900

    Implemented the Device class.

---
+++ ActiveStateMachine/ApplicationServices/Device.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ApplicationServices
+{
+    /// <summary>
+    /// Base class for all devices managed by device manager
+    /// </summary>
+    public abstract class Device
+    {
+        Action<string, string, string> _devEventMethod;
+        public string DevName { get; private set; }
+
+        public Device(string deviceName,
+            Action<string, string, string> eventCallBack)
+        {
+            DevName = deviceName;
+            _devEventMethod = eventCallBack;
+        }
+
+        // Device initialization method - need to be implemented in derived
+        // classes
+        public abstract void OnInit();
+
+        public void RegisterEventCallback(
+            Action<string, string, string> method)
+        {
+            _devEventMethod = method;
+        }
+
+        public void DoNotificationCallBack(
+            string name, string eventInfo, string source)
+        {
+            _devEventMethod?.Invoke(name, eventInfo, source);
+        }
+    }
+}
